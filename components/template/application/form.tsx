b0797f9eaa6bd5e2a737c667f1c3d19239c244bd
AndreQuintero <andre.quintero96@gmail.com>
2025-03-17 22:57:36 +0800

    delete: form types

--- components/template/application/form.tsx
+++ components/template/application/form.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useCallback, useEffect, useState } from "react";
-import { useForm, UseFormReturn } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
@@ -10,51 +10,14 @@ import { StudentsInformation } from "@/components/layout/application/students";
 import { WorksiteInformation } from "@/components/layout/application/worksite";
 import { SupervisorInformation } from "@/components/layout/application/supervisor";
 import { ScheduleInformation } from "@/components/layout/application/schedule";
+import { AccordionForms, ApplicationFormData, formSchema } from "@/app/services/application";
 
-export enum AccordionForms {
-    STUDENTS = "STUDENTS",
-    WORKSITE = "WORKSITE",
-    WORKSITE_SUPERVISOR = "WORKSITE_SUPERVISOR",
-    WORK_SCHEDULE = "WORK_SCHEDULE"
-}
-
-export const formSchema = z.object({
-    name: z.string().min(1, { message: "Name is required" }),
-    birthDate: z.date({ required_error: "Date of Birth is required" }),
-    email: z
-      .string()
-      .min(1, { message: "Email is required" })
-      .email({ message: "Please enter a valid email address" }),
-    organizationName: z.string().min(1, { message: "Church/Organization name is required" }),
-    webpage: z.string().optional(),
-    streetLine1: z.string().min(1, { message: "Street Line is required" }),
-    streetLine2: z.string().optional(),
-    streetLine3: z.string().optional(),
-    city: z.string().min(1, { message: "City name is required" }),
-    state: z.string().min(1, { message: "State name is required" }),
-    nation: z.string().min(1, { message: "Nation name is required" }),
-    zipCode: z.string().min(1, { message: "Zip code is required" }),
-    supervisorName: z.string().min(1, { message: "Supervisor name is required" }),
-    title: z.string().min(1, { message: "Title is required" }),
-    supervisorEmail: z.string()
-    .min(1, { message: "Supervisor email is required" })
-    .email({ message: "Please enter a valid email address" }),
-    supervisorPhone: z.string().min(1, { message: "Supervisor phone number is required" }),
-    startDate: z.date({ required_error: "Start Date is required" }),
-    endDate: z.date({ required_error: "End Date is required" }),
-    jobDescription: z.string().min(1, { message: "Job description is required" })
-
-  });
-
-export type FormApplicationProps = {
-    form: UseFormReturn<z.infer<typeof formSchema>>
-}
 
 export const ApplicationForm = () => {
    
     
       const [formsOpened, setFormsOpened] = useState<string[]>([AccordionForms.STUDENTS]);
-      const form = useForm<z.infer<typeof formSchema>>({
+      const form = useForm<ApplicationFormData>({
         resolver: zodResolver(formSchema),
         defaultValues: {
           name: "",
@@ -133,9 +96,22 @@ export const ApplicationForm = () => {
         }
       };
     
-      const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log("Form submitted successfully:", values);
-      };
+      const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await fetch("/api/application", {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
+            const content = await response.json()
+            console.log(content)
+        } catch(e) {
+            console.log(e)
+        }
+      }
     
       return (
         <Form {...form}>
