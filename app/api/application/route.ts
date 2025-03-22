0134b106e336cdc93ebc946b5f4ca06733ef23c3
AndreQuintero <andre.quintero96@gmail.com>
2025-03-23 01:24:06 +0800

    add: integration with google sheets and schema validation on server side

---
+++ app/api/application/route.ts
@@ -0,0 +1,74 @@
+import { ApplicationFormData, formSchema } from "@/app/services/application";
+import { auth } from "@/app/services/google";
+import { ErrorTypes } from "@/app/types/response";
+import { google, Auth } from "googleapis";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const body = (await request.json()) as ApplicationFormData
+    const parsedBody: ApplicationFormData = {
+        ...body,
+        birthDate: new Date(body.birthDate),
+        startDate: new Date(body.startDate),
+        endDate: new Date(body.endDate)
+    }
+    // Use Zod to validate the received data against the UserSchema
+    const result = formSchema.safeParse(parsedBody)
+  
+    // Check if the validation is successful
+    if (result.success) {
+        const values = [
+            [
+                parsedBody.name,
+                parsedBody.birthDate.toISOString(),
+                parsedBody.email,
+                parsedBody.organizationName,
+                parsedBody.webpage,
+                parsedBody.streetLine1,
+                parsedBody.streetLine2,
+                parsedBody.streetLine3,
+                parsedBody.city,
+                parsedBody.state,
+                parsedBody.nation,
+                parsedBody.zipCode,
+                parsedBody.supervisorName,
+                parsedBody.title,
+                parsedBody.supervisorEmail,
+                parsedBody.supervisorPhone,
+                parsedBody.startDate.toISOString(),
+                parsedBody.endDate.toISOString(),
+                parsedBody.jobDescription
+            ]
+        ]
+        try {
+            const client = await auth.getClient()
+            const googleSheets = google.sheets({ version: "v4", auth: client as Auth.OAuth2Client })
+            const id = process.env.SHEET_ID
+           
+            await googleSheets.spreadsheets.values.append({
+                auth,
+                range: "Sheet1!A1:B2",
+                spreadsheetId: id,
+                valueInputOption: "USER_ENTERED",
+                insertDataOption: "INSERT_ROWS",
+                requestBody: {
+                    values
+                }
+            })
+            return NextResponse.json({ success: true })
+        } catch{
+            return NextResponse.json(
+                { success: false, type: ErrorTypes.INTEGRATION_ERROR, errors: "Failed to submit data to Google Sheets. Please try again later." },
+                { status: 500 }
+            );
+        } 
+    }
+  
+    // If validation errors, map them into an object
+    const serverErrors = Object.fromEntries(
+      result.error?.issues?.map((issue) => [issue.path[0], issue.message]) || []
+    )
+  
+    // Respond with a JSON object containing the validation errors
+    return NextResponse.json({ success: false, type: ErrorTypes.SCHEMA_VALIDATION, errors: serverErrors }, {status: 400})
+  }
