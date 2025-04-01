cafc2825329ff9a41b9956b7a527eb0f9969f53a
AndreQuintero <andre.quintero96@gmail.com>
2025-04-01 19:15:38 +0800

    add: use agreement  hook onSubmit implementation

---
+++ app/hooks/useAgreement.tsx
@@ -0,0 +1,46 @@
+import { UseFormReturn } from "react-hook-form"
+import { AgreementFormData } from "../services/agreement"
+import { ErrorTypes, Response } from "../types/response"
+import { toast } from "sonner"
+
+export const useAgreement = ( form : UseFormReturn<AgreementFormData>, onSuccess?: () => void) => {
+        
+    const onSubmit = async (values: AgreementFormData) => {
+        try {
+            const response = await fetch("/api/agreement/submit", {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
+            const content: Response = await response.json()
+            console.log(content)
+            if(!content.success) {
+                if(content.type === ErrorTypes.INTEGRATION_ERROR) {
+                    toast.error(content.errors as string)
+                }
+                if(content.type === ErrorTypes.SCHEMA_VALIDATION) {
+                    const errors = content.errors as Record<string, string>
+                    Object.entries(errors).forEach(([key, value]) => {
+                        form.setError(key as keyof AgreementFormData, { type: "manual", message: value }); // Set error for each field
+                    })
+                }
+            } else {
+                form.reset()
+                toast.success("The form has been submitted successfully!")
+                if(onSuccess) {
+                    onSuccess()
+                }
+            }
+            
+        } catch(e) {
+            console.log('catch', e)
+        }
+    }
+
+    return {
+        onSubmit
+    }
+}
