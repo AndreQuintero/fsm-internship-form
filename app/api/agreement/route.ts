1d726097387e7a803e6107e6a54e841a92bb8e48
AndreQuintero <andre.quintero96@gmail.com>
2025-04-16 00:38:31 +0800

    add: temporary link rule

--- app/api/agreement/route.ts
+++ app/api/agreement/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server"
 import { v4 as uuidv4 } from 'uuid'
 
 export async function POST(request: Request) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const body = (await request.json()) as AgreementFormData
     const id = uuidv4()
 
