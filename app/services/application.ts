1784a5210a28082563f6ef2ccfb9107c21e02694
AndreQuintero <andre.quintero96@gmail.com>
2025-04-17 00:19:50 +0800

    move: disable days before today function to date service

--- app/services/application.ts
+++ app/services/application.ts
@@ -46,12 +46,3 @@ export type ApplicationFormData = z.infer<typeof formSchema>
 export type FormApplicationProps = {
     form: UseFormReturn<ApplicationFormData>
 }
-
-
-export const disableDaysBeforeToday = (date: Date) => {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); // Normalize today to midnight
-  const normalizedDate = new Date(date);
-  normalizedDate.setHours(0, 0, 0, 0); // Normalize the input date to midnight
-  return normalizedDate < today; // Disable only past dates (not today)
-}
