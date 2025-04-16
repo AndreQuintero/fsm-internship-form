e72563775389e888137d0d1ff153803e7f473282
AndreQuintero <andre.quintero96@gmail.com>
2025-04-17 00:21:39 +0800

    add: inserting form data into db and generating link

--- app/api/agreement/route.ts
+++ app/api/agreement/route.ts
@@ -1,14 +1,35 @@
+ 
 import { AgreementFormData } from "@/app/services/agreement"
 import { NextResponse } from "next/server"
 import { v4 as uuidv4 } from 'uuid'
+import { insertAgreement } from "@/app/services/db/agreement";
+
 
 export async function POST(request: Request) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const body = (await request.json()) as AgreementFormData
     const id = uuidv4()
+    try {
+        await insertAgreement(id, body)
+        const link = generateLink(id, request)
+        return NextResponse.json(
+            { success: true, link },
+            { status: 200 }
+        )
+    } catch (e) {
+        console.log(e)
+        return NextResponse.json(
+            { success: false },
+            { status: 500 }
+        )
+    }
+}
 
-    return NextResponse.json(
-        { success: true, link: id },
-        { status: 200 }
-    )
+const generateLink = (id: string, request: Request) => {
+    // Get the host from request headers
+    const host = request.headers.get('host');
+    const protocol = process.env.NODE_ENV === 'development' ? 'http://' : 'https://';
+    const baseUrl = `${protocol}${host}`;
+    
+    const agreementLink = `${baseUrl}/agreement?hash_id=${id}`;
+    return agreementLink
 }
