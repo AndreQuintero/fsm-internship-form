a1d85ec07b6b0d3b83709288157486e7fed3aaf0
AndreQuintero <andre.quintero96@gmail.com>
2025-04-16 13:54:51 +0800

    change: using list component

--- components/layout/agreement/terms-conditions.tsx
+++ components/layout/agreement/terms-conditions.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { List } from "@/components/ui/list"
 import { ReactNode } from "react"
 
 export type AgreementTermsCardProps = {
@@ -14,18 +15,7 @@ export const TermsAndConditions = ({children, terms = [], title}: AgreementTerms
                 <CardTitle>{title}</CardTitle>
             </CardHeader>
             <CardContent className="grid gap-4">
-                <div className="max-h-[200px] overflow-y-auto pr-2"> {/* Adjust max-height as needed */}
-                    {terms.map( term => (
-                        <div key={term} className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                            <div className="space-y-1">
-                                <p className="text-sm font-medium leading-none">
-                                    {term}
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <List items={terms} scrollable/>
             </CardContent>
             <CardFooter>
                 {children}                
