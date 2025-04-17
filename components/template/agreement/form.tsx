370d508ef6806fab83719e5f7cd66265def27f5e
AndreQuintero <andre.quintero96@gmail.com>
2025-04-18 00:35:03 +0800

    add: using data from the fetched data to set the default values

--- components/template/agreement/form.tsx
+++ components/template/agreement/form.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { useAgreement } from "@/app/hooks/useAgreement";
-import { AgreementFormData, formSchema } from "@/app/services/agreement";
+import { AgreementFormData, DEFAULT_VALUES, formSchema } from "@/app/services/agreement";
+import { AgreementData, convertAgreementDbDataToAgreementForm } from "@/app/services/db/agreement";
 import { Address } from "@/components/layout/agreement/address";
 import { GenerateLink } from "@/components/layout/agreement/generate-link";
 import { InternshipDate } from "@/components/layout/agreement/internship-date";
 import { PersonalInfo } from "@/components/layout/agreement/personal-info";
 import { Signatures } from "@/components/layout/agreement/signatures";
 import { TermsSection } from "@/components/layout/agreement/terms-conditions-section";
+import { buttonVariants } from "@/components/ui/button";
 import { ButtonWithLoading } from "@/components/ui/buttonWithLoading";
 import { Form } from "@/components/ui/form";
 import { SignaturePadRef } from "@/components/ui/signature-pad";
@@ -15,34 +17,30 @@ import { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 
-export const AgreementForm = () => {
+type AgreementFormProps = {
+    data: AgreementData | null
+}
+
+export const AgreementForm = ({ data }: AgreementFormProps) => {
+
+    //TODO: MAKE A PARENT COMPONENT THAT HANDLES THE FORM STATUS, IF IT'S NOT VALID, REDIRECT TO /agreement AND TOAST THE ERRORS
     const studentSigRef = useRef<SignaturePadRef>(null);
     const supervisorSigRef = useRef<SignaturePadRef>(null);
+    const getRetrievedData = () => {
+        return !data ? DEFAULT_VALUES : convertAgreementDbDataToAgreementForm(data)
+    }
+    
     const form = useForm<AgreementFormData>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-           name: "",
-           supervisorName: "",
-           title:"",
-           email: "",
-           phone: "",
-           organization: "",
-           address: "",
-           city: "",
-           state: "",
-           nation: "",
-           zipCode: "",
-           startDate: undefined,
-           endDate: undefined,
-           studentAgreement: false,
-           supervisorAgreement: false
+            ...getRetrievedData()
         },
     });
     const clearSignature = () => {
         studentSigRef.current?.clear();
         supervisorSigRef.current?.clear();  
     }
-    const { onSubmit } = useAgreement(form, clearSignature)
+    const { onSubmit } = useAgreement(form, clearSignature, data?.hash)
 
     return (
         <Form {...form}>
@@ -57,7 +55,9 @@ export const AgreementForm = () => {
                 <div className="flex gap-4 flex-wrap justify-center lg:justify-normal">
                     <ButtonWithLoading disabled={form.formState.isSubmitting} type="submit" isLoading={form.formState.isSubmitting} loadingText="Submitting..." text="Submit"/>
                     <span className="lg:mt-1">Or</span>
-                    <GenerateLink form={form}/>       
+                    {!data ? <GenerateLink form={form}/> : 
+                    <ButtonWithLoading className={`w-full lg:w-fit ${buttonVariants({ variant: 'secondary' })}`} type="button" isLoading={form.formState.isSubmitting} loadingText="Updating..." text="Update"/>
+                    }       
                 </div>
             </form>
         </Form> 
