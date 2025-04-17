8088521a9bdab7bd0be2d0550e0215319d00ba90
AndreQuintero <andre.quintero96@gmail.com>
2025-04-18 00:31:01 +0800

    add: fetch agreement data when theres hash id

--- app/agreement/page.tsx
+++ app/agreement/page.tsx
@@ -1,11 +1,28 @@
 import { AgreementForm } from "@/components/template/agreement/form";
 import { BaseTemplate } from "@/components/template/base";
+import { getAgreementByHashId } from "../services/db/agreement";
 
-export default function Agreement() {
+const fetchAgreementData = async (hash_id?: string) => {
+  if(!hash_id) return null
+  try {
+    return await getAgreementByHashId(hash_id)
+  } catch(e) {
+    console.log(e)
+  }
+  return null 
+} 
+
+type AgreementPageProps = {
+  searchParams: { hash_id?: string }
+}
+
+export default async function Agreement({ searchParams }: AgreementPageProps) {
+    const { hash_id } =  await searchParams
+    const agreementData = await fetchAgreementData(hash_id) 
      return (
         <main>
           <BaseTemplate title="Internship Agreement Form">
-            <AgreementForm />
+            <AgreementForm data={agreementData}/>
           </BaseTemplate>
         </main>
       );
