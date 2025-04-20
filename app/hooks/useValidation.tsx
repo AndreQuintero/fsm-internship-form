62263f1a92bec0e19ec3e3610ffcdb037511812e
AndreQuintero <andre.quintero96@gmail.com>
2025-04-20 21:56:36 +0800

    add: validation hook implementation

---
+++ app/hooks/useValidation.tsx
@@ -0,0 +1,34 @@
+import { DefaultValues, FieldValues, Path, UseFormReturn } from "react-hook-form";
+import { ErrorTypes, Response } from "../types/response";
+import { toast } from "sonner";
+
+export const useValidation = <T extends FieldValues>(form : UseFormReturn<T>) => {
+    const handleSchemaValidation = (response: Response) => {
+        const errors = response.errors as Record<string, string>
+        Object.entries(errors).forEach(([key, value]) => {
+            form.setError(key as Path<T>, { type: "manual", message: value }) // Set error for each field
+        })
+    }
+
+    const handleIntegrationValidation = (response: Response) => {
+        toast.error(response.errors as string)
+    }
+
+    const validate = (response: Response) => {
+        if(response.type === ErrorTypes.INTEGRATION_ERROR) {
+            handleIntegrationValidation(response)
+        }
+        if(response.type === ErrorTypes.SCHEMA_VALIDATION) {
+            handleSchemaValidation(response)
+        }
+    }
+
+    const success = (defaultValues?: DefaultValues<T>) => {
+        form.reset(defaultValues ?? undefined)
+        toast.success("The form has been submitted successfully!")
+    }
+    return {
+        validate,
+        success
+    }
+}
