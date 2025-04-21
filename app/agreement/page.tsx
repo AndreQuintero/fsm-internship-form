289ca55f9dcdc3356521c0783e5b1806a276b159
AndreQuintero <andre.quintero96@gmail.com>
2025-04-21 21:40:48 +0800

    add: handle status and agreement data implementation

--- app/agreement/page.tsx
+++ app/agreement/page.tsx
@@ -1,6 +1,7 @@
 import { AgreementForm } from "@/components/template/agreement/form";
 import { BaseTemplate } from "@/components/template/base";
-import { getAgreementByHashId } from "../services/db/agreement";
+import { AgreementData, getAgreementByHashId } from "../services/db/agreement";
+import { FormStatus } from "../services/form";
 
 const fetchAgreementData = async (hash_id?: string) => {
   if(!hash_id) return null
@@ -12,18 +13,30 @@ const fetchAgreementData = async (hash_id?: string) => {
   return null 
 } 
 
+const handleAgreementData = (data: AgreementData | null) => {
+  if(!data) return null
+  return data.form_status === FormStatus.VALID ? data : null
+}
+
 type AgreementPageProps = {
   searchParams: Promise<{ hash_id?: string }>
 }
 
 export default async function Agreement({ searchParams }: AgreementPageProps) {
     const { hash_id } =  await searchParams
-    const agreementData = await fetchAgreementData(hash_id) 
+    const agreementData = await fetchAgreementData(hash_id)
+    
+    const getStatus = (data: AgreementData | null): FormStatus => {
+      if (!data && !hash_id) return FormStatus.VALID
+      if (!data && hash_id) return FormStatus.INVALID
+      return data?.form_status ?? FormStatus.INVALID
+    }
+
      return (
         <main>
           <BaseTemplate title="Internship Agreement Form">
-            <AgreementForm data={agreementData}/>
+            <AgreementForm status={getStatus(agreementData)} data={handleAgreementData(agreementData)}/>
           </BaseTemplate>
         </main>
-      );
+      )
 }
