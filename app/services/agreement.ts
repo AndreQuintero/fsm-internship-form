ac4d8ce8d3618a03d85ba276e8612d1ac4c77ce8
AndreQuintero <andre.quintero96@gmail.com>
2025-04-21 22:35:49 +0800

    add: checking if form is expired

--- app/services/agreement.ts
+++ app/services/agreement.ts
@@ -1,4 +1,7 @@
 import { AgreementFormData } from "./agreement-form";
+import { addHoursToDate, isDateExpired } from "./date";
+import { AgreementData, convertAgreementDbDataToAgreementForm, updateFormExpired } from "./db/agreement";
+import { FormStatus } from "./form";
 import { setUrl } from "./hash";
 
 export const submitRequest = async (values: AgreementFormData, hash_id?: string) => {
@@ -24,3 +27,28 @@ export const generateLinkRequest = async (values: AgreementFormData) => {
     })
     return response
 }
+
+export const checkIfDateIsExpired = (createdAt: Date) => {
+    const targetDate = addHoursToDate(createdAt, Number(process.env.EXPIRATION_TIME!))
+    const currentDate = new Date()
+    return isDateExpired(currentDate, targetDate)
+}
+
+export const handleExpirationDate = async (data: AgreementData, hash: string) => {
+    if(checkIfDateIsExpired(new Date(data.created_at))) {
+        try {
+          await updateFormExpired(hash, convertAgreementDbDataToAgreementForm(data))
+          data.form_status = FormStatus.EXPIRED
+        } catch(e) {
+          console.log(e)
+        }
+      }
+      return data
+}
+
+export const checkExpiration = async (data: AgreementData | null) => {
+    if(!data) return null
+    if(data.form_status === FormStatus.VALID) return await handleExpirationDate(data, data.hash)
+    return data
+}
+  
