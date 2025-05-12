22328cf95b246c4fa96f5e28642bbb5fd3e06d13
AndreQuintero <andre.quintero96@gmail.com>
2025-05-12 20:59:40 +0800

    change:  section margin top

--- components/template/base.tsx
+++ components/template/base.tsx
@@ -10,10 +10,10 @@ type BaseTemplateProps = {
 
 export const BaseTemplate = ({ title, description, children}: BaseTemplateProps) => (
      <Container>
-        <section className="flex flex-col justify-center items-center mt-10 lg:mt-30 p-2.5">
+        <section className="flex flex-col justify-center items-center p-2.5">
             <Image src="/logo.png" alt="Fire School of Ministry" width={250} height={250} className=""/>
             <h1 className="font-bold text-4xl text-center lg:text-6xl">{title}</h1>
-            {!!description && <p className="text-primary text-center mt-10">{description}</p>}
+            {!!description && <p className="text-primary text-center mt-3">{description}</p>}
             <div className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
                 {children}
             </div>          
