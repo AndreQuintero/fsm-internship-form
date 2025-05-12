0ca6dd21c02a8f2038d69a397e2214e1cee0af3e
AndreQuintero <andre.quintero96@gmail.com>
2025-05-12 15:03:27 +0800

    add: using use device type hook to render the menus mobile or desktop

--- components/ui/menu.tsx
+++ components/ui/menu.tsx
@@ -1,24 +1,23 @@
 "use client"
-import Link from "next/link"
-import { NavigationMenu,NavigationMenuLink } from "./navigation-menu"
-import { usePathname } from "next/navigation"
+import { DeviceTypeEnum, useDeviceType } from "@/app/hooks/useDeviceType"
+import { MenuDesktop } from "./menu-desktop"
+import { MenuMobile } from "./menu-mobile"
+import { useEffect, useState } from "react"
 
 export const Menu = () => {
-    const pathname = usePathname()
-    const active = (route: string) => {
-        return route === pathname
+    const [mounted, setMounted] = useState(false)
+    const deviceType = useDeviceType()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
     }
     return (
-        <NavigationMenu>
-            <div className="flex space-x">
-                <Link href="/" passHref legacyBehavior>
-                    <NavigationMenuLink data-active={active("/")}>Application</NavigationMenuLink>
-                </Link>
-                <Link href="/agreement" passHref legacyBehavior>
-                    <NavigationMenuLink  data-active={active("/agreement")}>Agreement</NavigationMenuLink>
-                </Link>    
-                <NavigationMenuLink>Evaluation</NavigationMenuLink>
-            </div>
-        </NavigationMenu>
+        <header className="flex w-full">
+            {deviceType === DeviceTypeEnum.DESKTOP ? <MenuDesktop /> : <MenuMobile />}
+        </header>
     )
 }
