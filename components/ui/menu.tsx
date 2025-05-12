28f0e5f6df2a56bbf373eafab0e54f0f6d76552b
AndreQuintero <andre.quintero96@gmail.com>
2025-05-12 21:02:16 +0800

    change: using css to control the menus displays

--- components/ui/menu.tsx
+++ components/ui/menu.tsx
@@ -1,23 +1,15 @@
-"use client"
-import { DeviceTypeEnum, useDeviceType } from "@/app/hooks/useDeviceType"
 import { MenuDesktop } from "./menu-desktop"
 import { MenuMobile } from "./menu-mobile"
-import { useEffect, useState } from "react"
 
 export const Menu = () => {
-    const [mounted, setMounted] = useState(false)
-    const deviceType = useDeviceType()
-
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return null
-    }
     return (
-        <header className="flex w-full">
-            {deviceType === DeviceTypeEnum.DESKTOP ? <MenuDesktop /> : <MenuMobile />}
+        <header className="flex w-full justify-end">
+            <div className="hidden md:block">
+                <MenuDesktop />
+            </div>
+            <div className="block md:hidden">
+                <MenuMobile />
+            </div>
         </header>
     )
 }
