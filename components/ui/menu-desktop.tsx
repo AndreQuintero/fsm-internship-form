bf91cafb8f6c5ee1996e033aed9a67460af9e253
AndreQuintero <andre.quintero96@gmail.com>
2025-05-12 15:02:45 +0800

    add: moved menu content to  menu desktop file

---
+++ components/ui/menu-desktop.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { usePathname } from "next/navigation"
+import { NavigationMenu, NavigationMenuLink } from "./navigation-menu"
+import Link from "next/link"
+
+export const MenuDesktop = () => {
+    const pathname = usePathname()
+    const active = (route: string) => {
+        return route === pathname
+    }
+    return (
+        <NavigationMenu>
+            <div className="flex space-x">
+                <Link href="/" passHref legacyBehavior>
+                    <NavigationMenuLink data-active={active("/")}>Application</NavigationMenuLink>
+                </Link>
+                <Link href="/agreement" passHref legacyBehavior>
+                    <NavigationMenuLink  data-active={active("/agreement")}>Agreement</NavigationMenuLink>
+                </Link>    
+                <NavigationMenuLink>Evaluation</NavigationMenuLink>
+            </div>
+        </NavigationMenu>
+    )
+}
