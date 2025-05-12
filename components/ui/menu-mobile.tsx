f34b0a2ab4070a7a44a73ded7d5c21f8206f96bd
AndreQuintero <andre.quintero96@gmail.com>
2025-05-12 15:03:01 +0800

    add: menu mobile sidebar implementation

---
+++ components/ui/menu-mobile.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { NotebookPen, PanelRightOpen, PenLine } from "lucide-react"
+import { Sidebar, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarTrigger, useSidebar } from "./sidebar"
+import Link from "next/link"
+import { useEffect } from "react"
+import { usePathname } from "next/navigation"
+
+export const MenuMobile = () => {
+    const { setOpenMobile } = useSidebar()
+    const pathname = usePathname()
+  
+    useEffect(() => {
+      setOpenMobile(false);
+    }, [pathname, setOpenMobile])
+
+    const getActiveLinkClass = (route: string) => {
+        return route === pathname ? "font-bold" : "";
+    }
+    return(
+        <>
+            <div className="flex w-full justify-end">
+                <SidebarTrigger>
+                    <PanelRightOpen />
+                </SidebarTrigger>
+            </div>
+            <Sidebar side="right">
+                <SidebarGroup>
+                    <SidebarHeader>
+                        <SidebarGroupLabel>Fire School of Ministry</SidebarGroupLabel>
+                    </SidebarHeader>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            <SidebarMenuItem className="flex flex-col space-y-2.5">
+                                <SidebarMenuButton asChild>
+                                    <Link href="/" passHref legacyBehavior>
+                                        <span className={`flex gap-2 cursor-pointer ${getActiveLinkClass("/")}`}> <PenLine /> Application</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuButton asChild>
+                                    <Link href="/agreement" passHref legacyBehavior>
+                                        <span className={`flex gap-2 cursor-pointer ${getActiveLinkClass("/agreement")}`}> <NotebookPen /> Agreement</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </Sidebar>
+        </>
+    )
+}
