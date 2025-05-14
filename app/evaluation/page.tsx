cce082cf29b5d5d06d5d4de93dc27425cd0bd14f
AndreQuintero <andre.quintero96@gmail.com>
2025-05-14 22:57:43 +0800

    add: evaluation page initial implementation

---
+++ app/evaluation/page.tsx
@@ -0,0 +1,11 @@
+import { BaseTemplate } from "@/components/template/base";
+
+export default function Evaluation() {
+    return (
+        <main>
+          <BaseTemplate title="Student Evaluation Form">
+            <h1>Student Evaluation Form</h1>
+          </BaseTemplate>
+        </main>
+    )
+}
