4d14f64d558a9528956c0d9c0fc7c8da089080b3
AndreQuintero <andre.quintero96@gmail.com>
2025-05-15 20:35:06 +0800

    add: importing evaluation form

--- app/evaluation/page.tsx
+++ app/evaluation/page.tsx
@@ -1,10 +1,11 @@
 import { BaseTemplate } from "@/components/template/base";
+import { EvaluationForm } from "@/components/template/evaluation/form";
 
 export default function Evaluation() {
     return (
         <main>
           <BaseTemplate title="Student Evaluation Form">
-            <h1>Student Evaluation Form</h1>
+            <EvaluationForm />
           </BaseTemplate>
         </main>
     )
