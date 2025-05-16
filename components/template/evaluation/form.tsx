68dfed9567d19f11cc227cb531eb48adc764ebb4
AndreQuintero <andre.quintero96@gmail.com>
2025-05-16 22:34:56 +0800

    add: using on submit, on error, and sections functions

--- components/template/evaluation/form.tsx
+++ components/template/evaluation/form.tsx
@@ -1,10 +1,13 @@
 'use client'
 import { useAccordionFormErrors } from "@/app/hooks/useAccordionFormErrors";
+import { useEvaluation } from "@/app/hooks/useEvaluation";
 import { AccordionForms, DEFAULT_VALUES, evaluationAccordionErrorMapping, EvaluationFormData, formSchema } from "@/app/services/evaluation-form";
 import { Instructions } from "@/components/layout/evaluation/instructions";
 import { Personal } from "@/components/layout/evaluation/personal";
+import { ProfessionalSection } from "@/components/layout/evaluation/professional-section";
 import { Rubric } from "@/components/layout/evaluation/rubric";
 import { SpiritualSection } from "@/components/layout/evaluation/spiritual-section";
+import { SupervisionSection } from "@/components/layout/evaluation/supervision-section";
 import { Accordion } from "@/components/ui/accordion";
 import { ButtonWithLoading } from "@/components/ui/buttonWithLoading";
 import { Form } from "@/components/ui/form";
@@ -18,11 +21,12 @@ export const EvaluationForm = () => {
         defaultValues: DEFAULT_VALUES,
         mode: "all"
     })
-    const { errors } = form.formState
+    const { onSubmit, onSubmitError } = useEvaluation(form)
+    const { errors, isSubmitting } = form.formState
     const { formsOpened, setFormsOpened } = useAccordionFormErrors<EvaluationFormData, AccordionForms>(errors, evaluationAccordionErrorMapping, [AccordionForms.SPIRITUAL])
     return (
         <Form {...form}>
-            <form className="mt-10">
+            <form className="mt-10" onSubmit={form.handleSubmit(onSubmit, onSubmitError)}>
                 <div className="space-y-10">
                     <Personal form={form}/>
                     <Instructions />
@@ -30,8 +34,10 @@ export const EvaluationForm = () => {
                 </div>
                 <Accordion type="multiple" value={formsOpened} onValueChange={(value: string[]) => setFormsOpened(value as AccordionForms[])}>
                     <SpiritualSection form={form}/>
+                    <ProfessionalSection form={form}/>
+                    <SupervisionSection form={form}/>
                 </Accordion>
-                <ButtonWithLoading disabled={form.formState.isSubmitting} type="submit" isLoading={form.formState.isSubmitting} loadingText="Submitting..." text="Submit"/>
+                <ButtonWithLoading disabled={isSubmitting} type="submit" isLoading={isSubmitting} loadingText="Submitting..." text="Submit"/>
             </form>
         </Form>
     )
