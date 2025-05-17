a8f8529dc63c854676f0e43719a18d993d2ae1bf
AndreQuintero <andre.quintero96@gmail.com>
2025-05-17 18:31:31 +0800

    refactor(layout): restructure worksite form using FormItemWrapper and Row components
    Simplify form layout by replacing FormItem and FormControl with FormItemWrapper and organizing fields into rows for better readability and maintainability

--- components/layout/application/worksite.tsx
+++ components/layout/application/worksite.tsx
@@ -2,8 +2,10 @@ import { AccordionForms, FormApplicationProps } from "@/app/services/application
 import { AccordionItem, AccordionContent } from "@/components/ui/accordion";
 import { AccordionTitle } from "@/components/ui/accordion-title";
 import { CountryDropdown } from "@/components/ui/country-dropdown";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { FormField, FormMessage } from "@/components/ui/form";
+import { FormItemWrapper } from "@/components/ui/form-item-wrapper";
 import { Input } from "@/components/ui/input";
+import { Row } from "@/components/ui/row";
 
 export const WorksiteInformation = ({ form }: FormApplicationProps) => {
     return (
@@ -11,127 +13,101 @@ export const WorksiteInformation = ({ form }: FormApplicationProps) => {
             <AccordionTitle title="Worksite Information"/>
             <AccordionContent>
             <div className="space-y-8">
-                <FormField 
-                control={form.control}
-                name="organizationName"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Church/Organization name:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="webpage"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Website/Facebook page:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="streetLine1"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Street Line 1:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="streetLine2"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Street Line 2:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="streetLine3"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Street Line 3:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>City:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField 
-                control={form.control}
-                name="state"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>State:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField
-                    control={form.control}
-                    name="nation"
-                    render={({ field }) => (
-                        <FormItem>
-                        <FormLabel>Nation:</FormLabel>
-                        <CountryDropdown
-                            placeholder="Select a country"
-                            defaultValue={field.value}
-                            onChange={(country) => {
-                                field.onChange(`${country.emoji ?? ""} ${country.name}`)
-                            }}
-                        />
-                        <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField 
-                control={form.control}
-                name="zipCode"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Zip code:</FormLabel>
-                        <FormControl>
-                            <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
+                <Row>
+                    <FormField 
+                        control={form.control}
+                        name="organizationName"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Church/Organization name:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>       
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="webpage"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Website/Facebook page:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                </Row>
+                <Row cols="3">
+                    <FormField 
+                        control={form.control}
+                        name="streetLine1"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Street Line 1:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="streetLine2"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Street Line 2:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="streetLine3"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Street Line 3:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                </Row>
+                <Row>
+                    <FormField 
+                        control={form.control}
+                        name="city"
+                        render={({ field }) => (
+                            <FormItemWrapper label="City:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="state"
+                        render={({ field }) => (
+                            <FormItemWrapper label="State:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                </Row>
+                <Row>
+                    <FormField
+                        control={form.control}
+                        name="nation"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Country:" message={<FormMessage />}>
+                                <CountryDropdown
+                                    placeholder="Select a country"
+                                    defaultValue={field.value}
+                                    onChange={(country) => {
+                                        field.onChange(`${country.emoji ?? ""} ${country.name}`)
+                                    }}
+                                />
+                            </FormItemWrapper>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="zipCode"
+                        render={({ field }) => (
+                            <FormItemWrapper label="Zip Code:" message={<FormMessage />}>
+                                <Input {...field} />
+                            </FormItemWrapper>
+                        )}
+                    />
+                </Row>
             </div>
             </AccordionContent>
         </AccordionItem>
